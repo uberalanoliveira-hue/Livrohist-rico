18876fbe8937cddc4f0c5d9ef7be873f42814028
Alanoliveira <uber.alanoliveira@gmail.com>
2026-03-27 12:46:01 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'livro-historico-stable-rebuild-20260327-1';
+const CACHE_NAME = 'livro-historico-clean-refactor-1';
 const APP_ASSETS = ['./','./index.html','./app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
